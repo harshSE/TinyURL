de9c2fb9bc147a609baaaa86c0dbe283b2b3e50e
harshSE <h.rsh89@gmail.com>
2021-10-20 21:01:07 +0530

    setup project

---
+++ queryservice/src/test/java/com/harshse/queryservice/QueryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshse.queryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
